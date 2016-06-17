16edce58e92e81d4dc7eb185c6971fce61ec0d8c
Rémy HUBSCHER <rhubscher@mozilla.com>
2016-06-17 12:11:56 +0100

    Indentation fixes.

--- client/index.js
+++ client/index.js
@@ -3,15 +3,13 @@ var text;
 
 navigator.serviceWorker.register('service-worker.js')
   .then(function(registration){
-		return registration.pushManager.getSubscription().then(
-			function(subscription){
-				if (subscription)
-				{
-					return subscription;
-				}
-				return registration.pushManager.subscribe({ userVisibleOnly: true });
-			});
-
-	}).then(function(subscription){
+    return registration.pushManager.getSubscription()
+      .then(function(subscription) {
+        if (subscription) {
+          return subscription;
+        }
+        return registration.pushManager.subscribe({ userVisibleOnly: true });
+      });
+  }).then(function(subscription){
     console.log(JSON.stringify(subscription.toJSON()));
-	});
+  });
