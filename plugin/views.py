65d876bcb92eda3bb2a955ba5876191c9310d85a
ipsha21 <ipsha21@gmail.com>
2016-07-12 18:46:26 +0530

    updates views.py

--- plugin/views.py
+++ plugin/views.py
@@ -5,10 +5,12 @@ class PushURL(colander.MappingSchema):
     endpoint = colander.SchemaNode(colander.String(), validator = colander.url)
     key = colander.SchemaNode(colander.String())
 
-class subscriptionSchema(colander.MappingSchema):
-    subsID = colander.SchemaNode(colander.String())
+class Data(colander.MappingSchema):
     pushURL = PushURL()
     trigger = colander.SchemaNode(colander.String())
+
+class subscriptionSchema(colander.MappingSchema):
+    data = Data()
 
 @resource.register(name='subscriptioninfo')
 class Subscription(resource.UserResource):
